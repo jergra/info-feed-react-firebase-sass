bd8c1926045342f0ecf33655be9abdc735d3ff56
jergra <jergra43@gmail.com>
2022-11-23 22:39:49 -0800

    github logout icons

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -51,16 +51,20 @@ const Sidebar = ({ sidebar, handleToggleSidebar }) => {
             </li> */}
          </Link>
          
-            {/* <MdExitToApp size={23} /> */}
             <div className='header1__icons'>
                <img src={user?.photoURL} alt='' />
             </div>
-            <a>
-               <li onClick={logOutHandler}>
-                  <div style={{fontSize: 23, color: '#b1bdb4'}}>Logout</div>
-               </li>
-            </a>
-            <div className='header1__icons' style={{marginTop: 250}}>
+            
+            <div>
+               <a onClick={logOutHandler}>
+                  <div style={{display: 'flex', flexDirection: 'row'}}>
+                     <div><MdExitToApp size={23} /></div>
+                     <div style={{fontSize: 23, color: '#b1bdb4', marginLeft: -60}}>Logout</div>
+                  </div>
+               </a>
+            </div>
+            
+            <div className='header1__icons' style={{marginTop: 200}}>
                <a href='https://github.com/jergra/info-feed-react-firebase-sass' target='_blank' rel="noreferrer">
                   <AiFillGithub size={40} />
                </a>
